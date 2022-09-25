18c261690d9d637c11ff97b258338645438dcb2e
Amadu Jallow <aj22011021@utg.edu.gm>
2022-09-25 23:06:24 +0000

    My initial commit added the delete and size method

--- LinkedLst.py
+++ LinkedLst.py
@@ -13,7 +13,6 @@ class LinkedList:
     def push(self, data):
         if self._isempty():
             self._first = self.Node(data)
-
         else:
             node = self.Node(data)
             node.next = self._first
@@ -23,11 +22,25 @@ class LinkedList:
     def _isempty(self):
         return self._size == 0
 
+    def delete_head(self):
+        if self._isempty():
+            return
+        elif self._size == 1:
+            self._first = None
+            self._first.next = None
+            self._size = 0
+        else:
+            self._first = self._first.next
+            self._size -= 1
+
     def display(self):
         current = self._first
         while current is not None:
             print(f"Current:{current.data}")
             current = current.next
+
+    def size(self):
+        return self._size
 
 
 user = LinkedList()
@@ -38,4 +51,7 @@ user.push(30)
 user.push(44)
 user.push(363)
 user.push(360)
+user.delete_head()
+user.delete_head()
 user.display()
+print(user.size())
